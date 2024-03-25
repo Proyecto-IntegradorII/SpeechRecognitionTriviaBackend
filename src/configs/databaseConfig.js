4af5deaa583782f049d7ec7da50e6668711575b5
David Moreno <davidan7845@gmail.com>
2024-03-25 14:22:17 -0500

    cambio de base

--- src/configs/databaseConfig.js
+++ src/configs/databaseConfig.js
@@ -2,7 +2,7 @@ const { createClient } = require("@supabase/supabase-js");
 
 // Credenciales supabase
 console.log(process.env.SUPABASE_KEY)
-const supabaseUrl = "https://irsekyuuxfezyqafyihw.supabase.co";
+const supabaseUrl = "https://wkaiorpxhqggqwgojogj.supabase.co";
 const supabaseKey = process.env.SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
