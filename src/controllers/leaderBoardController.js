36744ae512a9d272479032e4e4f158b78aadf369
David Moreno <davidan7845@gmail.com>
2024-03-28 15:31:35 -0500

    cambio

--- src/controllers/leaderBoardController.js
+++ src/controllers/leaderBoardController.js
@@ -51,8 +51,42 @@ async function updateScore(req, res) {
     }
 }
 
+async function je(req, res) {
+	try {
+		// Datos obtenidos por el frontend
+		const { email, password } = req.body;
 
+		// Realizar la consulta para obtener todos los datos del usuario en la base de datos
+		const userData = await getUserByEmail(email);
+
+		// Verificar el hash de la contraseña
+		const passwordHash = userData.password;
+
+		// Comparar el hash almacenado con el hash de la contraseña proporcionada por el usuario
+		const match = await bcrypt.compare(password, passwordHash);
+
+		// Si las contraseñas no coinciden, se envía una respuesta de error
+		if (!match) {
+			throw new Error("Credenciales de inicio de sesión inválidas");
+		}
+
+		const user = {
+			id: userData.id,
+			email: userData.email,
+		};
+
+		// Generar token JWT con el user_id email y nickname del usuario
+		const token = jwt.sign(user, secretKey);
+
+		// Enviar el token al frontend con los datos del usuario y un mensaje de confirmacion
+		res.json({ userData, token, message: "Inicio de sesión exitoso" });
+	} catch (error) {
+		//console.error("Error al iniciar sesión:", error);
+		res.status(500).json({ error: "Credenciales de inicio de sesión inválidas" });
+	}
+}
 
 module.exports = {
-	updateScore
+	updateScore,
+	je
 };
--- src/routes/leaderBoardRoutes.js
+++ src/routes/leaderBoardRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const {
-	updateScore
+	updateScore,
+	je
 } = require("../controllers/leaderBoardController");
 
 router.post("/updatescore", updateScore);
+router.post("/je",je)
 
 module.exports = router;
